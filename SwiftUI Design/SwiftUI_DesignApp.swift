7221e3c9a9036ad70fc12d2b044dec4b44779a2c
Doğukan Çatmakaş <dcatmakas@gmail.com>
2023-05-05 12:05:05 +0300

    Initial Commit

---
+++ SwiftUI Design/SwiftUI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignApp.swift
+//  SwiftUI Design
+//
+//  Created by Doğukan Çatmakaş on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
